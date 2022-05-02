8c8c2f3a6a6219b86df5e04a55f290f4b6a72c79
Claire Carden <smclairecarden@gmail.com>
2022-05-02 17:37:32 -0500

    get X and O on board

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,9 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
-  styleUrls: ['./square.component.scss']
-  
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
